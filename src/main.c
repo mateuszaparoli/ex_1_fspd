063516c45c1d93fa8e939891b8a53dbee01ad1d4
mateuszaparoli <mateuszaparoli7@gmail.com>
2026-05-01 19:12:40 -0300

    add: começa a escrever o trabaioDaThread

--- src/main.c
+++ src/main.c
@@ -3,6 +3,90 @@
 #include <time.h>
 #include <stdlib.h>
 
+#define NUM_MAX_SALAS 10
+#define NUM_MAX_THREADS 30
+#define NUM_INFO_BASICAS_DA_THREAD 3
+
+#define NUM_MAXIMO_DE_SALAS_VISITADAS 60
+#define NUM_DE_INFO_POR_VISITA 2
+
+int matrizDeControleDasThreads[NUM_MAX_THREADS][NUM_INFO_BASICAS_DA_THREAD];
+
+int matrizDeVisitasDasThreads[NUM_MAX_THREADS][NUM_MAXIMO_DE_SALAS_VISITADAS][NUM_DE_INFO_POR_VISITA];
+
+int numSalas, numThreads;
+
+void passa_tempo(int tid, int sala, int decimos);
+
+void* trabaioDaThread(void* arg);
+
+int main(int argc, char** argv) {
+  
+    // leituras iniciais para criação da matriz de controle de threads
+    scanf("%d", &numSalas);
+    scanf("%d", &numThreads);
+    printf("%d %d\n", numSalas, numThreads);
+    
+    // inicialização da matriz de controle das threads
+    for (int i = 0; i < NUM_MAX_THREADS; i++) {
+        for (int j = 0; j < NUM_INFO_BASICAS_DA_THREAD; j++) {
+            matrizDeControleDasThreads[i][j] = 0;
+        }
+    }
+
+    // armazenando as informações das threads
+    int threadId;
+    int tempoInicialDaThread;
+    int qtdSalasVisitadasPelaThread;
+    for (int i = 0; i < numThreads; i++) {
+        scanf("%d", &threadId);
+        scanf("%d", &tempoInicialDaThread);
+        scanf("%d", &qtdSalasVisitadasPelaThread);
+        matrizDeControleDasThreads[i][0] = threadId;
+        matrizDeControleDasThreads[i][1] = tempoInicialDaThread;
+        matrizDeControleDasThreads[i][2] = qtdSalasVisitadasPelaThread;
+
+        for (int j = 0; j < qtdSalasVisitadasPelaThread; j++){
+            int idDaSala;
+            int tempoMinimoNaPosicao;
+            scanf("%d", &idDaSala);
+            scanf("%d", &tempoMinimoNaPosicao);
+            matrizDeVisitasDasThreads[i][j][0] = idDaSala;
+            matrizDeVisitasDasThreads[i][j][1] = tempoMinimoNaPosicao;
+        }
+    }
+
+    // Criação das Threads
+    pthread_t nossasThreads[numThreads];
+    for (int i = 0; i < numThreads; i++) {
+        if(pthread_create(&nossasThreads[i], NULL, trabaioDaThread, (void*)i) != 0) {
+            perror("Erro pra cria a thread");
+            exit(1);
+        }
+    }
+    
+    // Prints para depurar o trem
+    // for (int i = 0; i < numThreads; i++) {
+    //     for (int j = 0; j < NUM_INFO_BASICAS_DA_THREAD; j++) {
+    //         printf("%d ", matrizDeControleDasThreads[i][j]);
+    //     }
+    //     printf("\n");
+    //         for (int m = 0; m < matrizDeControleDasThreads[i][2]; m++) {
+    //                 printf("%d %d \n", 
+    //                     matrizDeVisitasDasThreads[i][m][0], 
+    //                     matrizDeVisitasDasThreads[i][m][1]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // Juntando as threads para esperarem que todas acabem antes do programa encerrar
+    for(int i = 0; i < numThreads; i++) {
+        pthread_join(nossasThreads[i], NULL);
+    }
+
+    return 0;
+}
+
 void passa_tempo(int tid, int sala, int decimos)
 {
     struct timespec zzz, agora;
@@ -36,71 +120,19 @@ void passa_tempo(int tid, int sala, int decimos)
     printf("%3d ) %2d @%2d\n",tstamp,tid,sala);
 }
 
-
-int main(int argc, char** argv) {
-  
-    // Declarações e leituras iniciais para criação da matriz de controle de threads
-    int numSalas, numThreads;
-    scanf("%d", &numSalas);
-    scanf("%d", &numThreads);
-    printf("%d %d\n", numSalas, numThreads);
+void* trabaioDaThread(void* arg) {
     
-    // Matriz de controle das threads
-    #define NUM_MAX_SALAS 30
-    #define NUM_MAX_THREADS 10
-    #define NUM_INFO_BASICAS_DA_THREAD 3
+    long indiceDaThread = (long) arg;
+
+    int idDaThread = matrizDeControleDasThreads[indiceDaThread][0];
+    int tempoDeEspera = matrizDeControleDasThreads[indiceDaThread][1];
+    int quantidadeDeSalasPraVisitar = matrizDeControleDasThreads[indiceDaThread][2];
+
+    passa_tempo(idDaThread, 0, tempoDeEspera);
     
-    int matrizDeControleDasThreads[NUM_MAX_THREADS][NUM_INFO_BASICAS_DA_THREAD];
-    for (int i = 0; i < NUM_MAX_THREADS; i++) {
-        for (int j = 0; j < NUM_INFO_BASICAS_DA_THREAD; j++) {
-            matrizDeControleDasThreads[i][j] = 0;
-        }
+    for (int k = 0; k < quantidadeDeSalasPraVisitar; k++) {
+        
     }
 
-    // Matriz de visitas da thread
-    #define NUM_MAXIMO_DE_SALAS_VISITADAS 1000
-    #define NUM_DE_INFO_POR_VISITA 2
-    int (*matrizDeVisitasDasThreads)[NUM_MAXIMO_DE_SALAS_VISITADAS][NUM_DE_INFO_POR_VISITA] = 
-        malloc(NUM_MAX_THREADS * sizeof(matrizDeVisitasDasThreads));
-
-    // Armazenando as informações das threads
-    int threadId;
-    int tempoInicialDaThread;
-    int qtdSalasVisitadasPelaThread;
-    for (int i = 0; i < numThreads; i++) {
-        scanf("%d", &threadId);
-        scanf("%d", &tempoInicialDaThread);
-        scanf("%d", &qtdSalasVisitadasPelaThread);
-        matrizDeControleDasThreads[i][0] = threadId;
-        matrizDeControleDasThreads[i][1] = tempoInicialDaThread;
-        matrizDeControleDasThreads[i][2] = qtdSalasVisitadasPelaThread;
-
-        for (int j = 0; j < qtdSalasVisitadasPelaThread; j++){
-            int idDaSala;
-            int tempoMinimoNaPosicao;
-            scanf("%d", &idDaSala);
-            scanf("%d", &tempoMinimoNaPosicao);
-            matrizDeVisitasDasThreads[i][j][0] = idDaSala;
-            matrizDeVisitasDasThreads[i][j][1] = tempoMinimoNaPosicao;
-        }
-
-    }
-
-
-    // Prints for debbuggin
-    for (int i = 0; i < numThreads; i++) {
-        for (int j = 0; j < NUM_INFO_BASICAS_DA_THREAD; j++) {
-            printf("%d ", matrizDeControleDasThreads[i][j]);
-        }
-        printf("\n");
-            for (int m = 0; m < matrizDeControleDasThreads[i][2]; m++) {
-                    printf("%d %d \n", 
-                        matrizDeVisitasDasThreads[i][m][0], 
-                        matrizDeVisitasDasThreads[i][m][1]);
-        }
-        printf("\n");
-    }
-
-    free(matrizDeVisitasDasThreads);
-    return 0;
+    return NULL;
 }
